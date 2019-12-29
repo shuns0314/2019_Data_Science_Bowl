88f871d7d19d392a79f9bb2469c2bcb1d3fffd40
shuns0314 <shunsuke.naka0314@gmail.com>
2019-12-29 11:33:58 +0000

    Add lgb regression models.

--- src/models/train.py
+++ src/models/train.py
@@ -1,13 +1,46 @@
+import argparse
+
 import numpy as np
 import pandas as pd
+import lightgbm as lgb
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_squared_error
 
 
-RAW_PATH = "/data/raw"
+parser = argparse.ArgumentParser()
+parser.add_argument('train_csv', type=str)
 
 
 def main():
     """main."""
-    train_df = pd.read_csv(f"{RAW_PATH}/train.csv")
-    train_labels_df = pd.read_csv(f"{RAW_PATH}/train_labels.csv")
-    specs_df = pd.read_csv(f"{RAW_PATH}/specs.csv")
-    
+    args = parser.parse_args()
+    train_df = pd.read_csv(f"data/processed/{args.train_csv}.csv", index_col=0)
+    # print(train_df.head())
+    y = train_df['accuracy_group']
+    x = train_df.drop('accuracy_group', axis=1)
+    lgb_regression(x, y)
+
+
+def lgb_regression(x, y):
+    x_train, x_test, y_train, y_test = train_test_split(x, y)
+    lgb_train = lgb.Dataset(x_train, y_train)
+    lgb_val = lgb.Dataset(x_test, y_test, reference=lgb_train)
+    print(x_train.columns)
+    lgb_params = {
+        'objective': 'regression',
+        'metric': 'rmse',
+    }
+
+    model = lgb.train(params=lgb_params,
+                      train_set=lgb_train,
+                      valid_sets=lgb_val)
+
+    y_pred = model.predict(x_test, num_iteration=model.best_iteration)
+    mse = mean_squared_error(y_test, y_pred)
+    rmse = np.sqrt(mse)
+    print(rmse)
+    print(y_pred)
+
+
+if __name__ == "__main__":
+    main()
