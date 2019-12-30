972e08fac74d1bb81f5dd24c605df8c82a9348ac
shuns0314 <shunsuke.naka0314@gmail.com>
2019-12-30 16:58:15 +0000

    Add save function of model.

--- src/models/train.py
+++ src/models/train.py
@@ -1,22 +1,22 @@
 import argparse
+import os
+from datetime import datetime
+import pickle
 
 import numpy as np
 import pandas as pd
 import lightgbm as lgb
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_squared_error
-import optuna
+import joblib
 
 from loss_function import qwk
-from sklearn.externals import joblib
-
-
-# load model
-# gbm_pickle = joblib.load('lgb.pkl')
+from post_process import post_processing, threshold
 
 
 parser = argparse.ArgumentParser()
 parser.add_argument('train_csv', type=str)
+parser.add_argument('--name', type=str)
 
 
 def main():
@@ -30,14 +30,25 @@ def main():
     x_train, x_val, y_train, y_val = train_test_split(x, y, test_size=0.15)
     model, params = lgb_regression(x_train, y_train)
     y_pred = model.predict(x_val)
-    y_pred = threshold(y_pred, params)
-
+    # print(type(y_pred))
     func = np.frompyfunc(threshold, 2, 1)
     post_pred = func(y_pred, params)
     loss = qwk(y_val, post_pred)
-    print(f"val_loss* {loss}")
+    print(f"val_loss: {loss}")
 
-    # joblib.dump(model, 'lgb.pkl')
+    if args.name is None:
+        now = datetime.now().strftime('%Y%m%d_%H%M%S')
+        args.name = f'lgb_{args.train_csv}_{now}'
+
+    if not os.path.exists(f'models/{args.name}'):
+        os.makedirs(f'models/{args.name}')
+
+    # modelのsave
+    joblib.dump(model, f'models/{args.name}/model_{args.name}.pkl')
+
+    # paramsのsave
+    with open(f'models/{args.name}/params_{args.name}.pkl', 'wb') as handle:
+        pickle.dump(params, handle)
 
 
 def lgb_regression(x: pd.DataFrame, y: pd.Series) -> pd.DataFrame:
@@ -62,49 +73,6 @@ def lgb_regression(x: pd.DataFrame, y: pd.Series) -> pd.DataFrame:
     params = post_processing(y_val, y_pred)
 
     return model, params
-
-
-def post_processing(y_test, y_pred):
-
-    def objectives(trial):
-        params = {
-            'threshold_0': trial.suggest_uniform('threshold_0', 0.0, 3.0),
-            'threshold_1': trial.suggest_uniform('threshold_1', 0.0, 3.0),
-            'threshold_2': trial.suggest_uniform('threshold_2', 0.0, 3.0),
-        }
-        func = np.frompyfunc(threshold, 2, 1)
-        post_pred = func(y_pred, params)
-        loss = qwk(y_test, post_pred)
-
-        return loss
-
-    study = optuna.create_study(direction='maximize')
-    study.optimize(objectives, n_trials=100)
-
-    print(f'Number of finished trials: {len(study.trials)}')
-
-    print('Best trial:')
-    trial = study.best_trial
-
-    print(f'  Value: {trial.value}')
-
-    print(f'  Params: ')
-    for key, value in trial.params.ite2ms():
-        print(f'    {key}: {value}')
-
-    return trial.params.ite2ms()
-
-# test
-def threshold(x, params):
-    if x < params['threshold_0']:
-        y = 0
-    elif x < params['threshold_1']:
-        y = 1
-    elif x < params['threshold_2']:
-        y = 2
-    else:
-        y = 3
-    return y
 
 
 if __name__ == "__main__":
