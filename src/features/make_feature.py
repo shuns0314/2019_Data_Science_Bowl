515304427995e5412ac132d3a5ba720179f205ef
shuns0314 <shunsuke.naka0314@gmail.com>
2019-12-30 10:39:43 +0000

    test

--- src/features/make_feature.py
+++ src/features/make_feature.py
@@ -23,6 +23,7 @@ class GetData():
 
         get_assesments = GetAssessmentFeature(self.win_code,
                                               test_set=self.test_set)
+        # test
 
         # まずgame_sessionでgroupbyする
         for i, session in user_sample.groupby('game_session', sort=False):
