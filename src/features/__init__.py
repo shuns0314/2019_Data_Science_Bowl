9444161c0d87d500288e830d376d728ea8c0ca63
shuns0314 <shunsuke.naka0314@gmail.com>
2020-01-18 08:49:12 +0000

    Add __init__.py

--- src/features/__init__.py
+++ src/features/__init__.py
@@ -3,3 +3,8 @@ from .make_feature import (  # noqa: F401
     GetData,
     GetAssessmentFeature
 )
+from .preprocess_feature_main import (  # noqa: F401
+    PreprocessTime,
+    MutualPreprocessFeatures,
+    PreprocessFeatures,
+)
