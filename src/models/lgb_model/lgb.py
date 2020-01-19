19d9694785f01edfe5a9f129145ade9360edca34
shuns0314 <shunsuke.naka0314@gmail.com>
2020-01-19 14:55:38 +0000

    delete target encoding

--- src/models/lgb_model/lgb.py
+++ src/models/lgb_model/lgb.py
@@ -1,8 +1,8 @@
 import pandas as pd
 import lightgbm as lgb
 
-from models.loss_function import lgb_qwk
-from models.target_encoding import TargetEncoding
+from src.models.loss_function import lgb_qwk
+from src.models.target_encoding import TargetEncoding
 
 
 FEATURES_FOR_TARGET_ENCODING = [
@@ -28,17 +28,17 @@ def lgb_model(x, y, train_ind, test_ind, test_x, seed, val_x=None):
     x_test = x.iloc[test_ind]
     y_test = y.iloc[test_ind]
 
-    target_encoding = TargetEncoding()
-    if val_x is not None:
-        x_train, x_test, test_x, val_x = target_encoding.process(
-            x_train, y_train, x_test, test_x,
-            columns=FEATURES_FOR_TARGET_ENCODING,
-            seed=seed, val_x=val_x)
-    else:
-        x_train, x_test, test_x = target_encoding.process(
-            x_train, y_train, x_test, test_x,
-            columns=FEATURES_FOR_TARGET_ENCODING,
-            seed=seed)
+    # target_encoding = TargetEncoding()
+    # if val_x is not None:
+    #     x_train, x_test, test_x, val_x = target_encoding.process(
+    #         x_train, y_train, x_test, test_x,
+    #         columns=FEATURES_FOR_TARGET_ENCODING,
+    #         seed=seed, val_x=val_x)
+    # else:
+    #     x_train, x_test, test_x = target_encoding.process(
+    #         x_train, y_train, x_test, test_x,
+    #         columns=FEATURES_FOR_TARGET_ENCODING,
+    #         seed=seed)
 
     lgb_train = lgb.Dataset(x_train, y_train)
     lgb_val = lgb.Dataset(x_test, y_test, reference=lgb_train)
